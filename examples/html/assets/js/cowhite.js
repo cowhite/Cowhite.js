35dc742be306634d8989e95bb8203fa393dc7e14
Bhaskar <bhaskar@cowhite.com>
2017-12-21 22:24:24 +0530

    Show submit form. Switch tab auto-fetches initial page.

--- examples/html/assets/js/cowhite.js
+++ examples/html/assets/js/cowhite.js
@@ -1,12 +1,25 @@
 function switchTab(tab) {
   console.log("Switchingggg tab to "+tab);
+  var url, $section;
   if(tab){
+    if(tab[0] == "!") {
+      return;
+    }
     if(!$("#"+tab+"-section").length){
       return;
     }
     $(".each-section").addClass("hide");
-    $("#"+tab+"-section").removeClass("hide");
+
+    $section = $("#"+tab+"-section");
+    $section.removeClass("hide");
     $(document).trigger(tab+"-opened");
+
+    url = $section.data("url");
+    if(url) {
+      $.get(url, function(res) {
+        $("#"+tab+"-content").html(res);
+      });
+    }
   }
 
 }
@@ -46,6 +59,66 @@ function onLoad() {
   var hashObj = getObjFromUrlHash();
   switchTab(hashObj.name);
 }
+
+var submitForm = function($this){
+    var modal = $('.form-modal'), showFormBtnId, $showFormBtn, url,
+      id, form, formContentId, modal, aftersuccess, type;
+
+    if($this.hasClass("btn-submit-form")) {
+      showFormBtnId = "#"+modal.find(".btn.btn-primary").data("btn-show-form-id");
+      $showFormBtn = $(showFormBtnId);
+    }else{
+      modal.find(".btn.btn-primary").data("btn-show-form-id", $this.attr('id'));
+    }
+    if(showFormBtnId){
+        url = $showFormBtn.data('submit-url'),
+        id = showFormBtnId,
+        form = modal.find(".modal-body form"),
+        formContentId = modal.find(".modal-body"),
+        afterSuccess = $showFormBtn.data('aftersuccess'),
+        type = "POST";
+    }else{
+        url = $this.data('url'),
+        id = $this.data('id'),
+        form = modal.find(".modal-body form"),
+
+        formContentId = modal.find(".modal-body"),
+        afterSuccess = $this.data('aftersuccess'),
+        type = "GET";
+    }
+
+    var data = new FormData($(form)[0]);
+
+    var width = $this.data("width");
+    if (!width){ width = "150px"; }
+
+    $.ajax({
+        url: url,
+        type: type,
+        data: data,
+        processData: false,
+        contentType: false,
+        success: function(res) {
+            if(res.error) {
+                $(formContentId).html(res.html);
+            }
+            else{
+
+                if($this.hasClass("btn-submit-form")){
+                    $(modal).modal('hide');
+                    eval(afterSuccess)();
+                }
+                else{
+                    $(formContentId).html(res);
+                }
+                if(type == 'GET') {
+                  modal.modal("show");
+                }
+            }
+        }
+    });
+}
+
 $(function(){
 
   onLoad();
@@ -58,6 +131,8 @@ $(function(){
       }
   });
 
-
+  $(document).on("click",".btn-show-submit-form", function(){
+      submitForm($(this));
+  });
 
 });
