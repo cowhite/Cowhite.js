7ce8ee0fdfbf54736877972e3bf6b9fe14ebec52
Bhaskar <bhaskar@cowhite.com>
2017-12-21 20:34:33 +0530

    Hashobj.

--- examples/html/assets/js/cowhite.js
+++ examples/html/assets/js/cowhite.js
@@ -10,8 +10,45 @@ function switchTab(tab) {
   }
 
 }
+function getObjFromUrlHash() {
+  var hash = window.location.hash,
+    obj = {}, hashName, hashArr, hashParams, hashParamsArr, hashParamsObj = {};
 
+  if(!hash) {
+    return {};
+  }
+  hashArr = hash.split("?");
+  if(hashArr.length) {
+    hashName = hashArr[0];
+    hashName = hashName.substr(1, hashName.length);
+  }
+  if(hashArr.length > 1) {
+    hashParams = hashArr[1];
+  }
+
+  if (hashParams){
+    hashParamsObj = {};
+    hashParamsArr = hashParams.split("&");
+      for(var i=0; i<hashParamsArr.length; i++){
+        temp = hashParamsArr[i].split("=");
+        hashParamsObj[temp[0]] = temp[1];
+      }
+  }
+  res = {
+    "name": hashName,
+    "params": hashParamsObj
+  }
+  return res;
+
+}
+
+function onLoad() {
+  var hashObj = getObjFromUrlHash();
+  switchTab(hashObj.name);
+}
 $(function(){
+
+  onLoad();
   $(window).on("hashchange", function() {
       var hash = window.location.hash;
       if(hash) {
